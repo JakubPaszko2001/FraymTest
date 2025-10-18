f8716342eee725f90c6cc2a6f13d8446052a4927
P4cho <jakubpaszko01@gmail.com>
2025-10-18 12:43:10 +0200

    Section Name Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 import AboutSection from './components/AboutSection';
 import AccordionSection from './components/AccordionSection';
-import Test2 from './components/Test2';
+import MainSection from './components/MainSection';
 import HowWeWork from './components/HowWeWork';
 import ContactPage from './components/ContactPage';
 
 export default function Page() {
   return (
     <>
-      <Test2 />
+      <MainSection />
       <AboutSection />
       <AccordionSection />
       <HowWeWork />
